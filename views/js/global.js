646271fc1f8bd632d3cd3f11a4b4563616022d2d
Georgiafab <2369596999@qq.com>
2024-06-18 10:06:14 +0800

    case pages

--- views/js/global.js
+++ views/js/global.js
@@ -14,22 +14,58 @@ innerloader = {
         }
         setTimeout(function () {
           $(".innerload").addClass("show");
-          setTimeout(function () {
-            $(".innerload").css("opacity", 0);
-            $("html").scrollTop(4);
-          }, 600);
+          // setTimeout(function () {
+          //   $(".innerload").css("opacity", 0);
+          //   // $("html").scrollTop(4);
+          // }, 600);
         }, 2000);
       }
     }
     a();
   },
 };
+//ScrollTrigger.config({ ignoreMobileResize: true });
+("use strict"); // fix lenis in safari
+const lenis = new Lenis({
+  lerp: 0.3,
+  wheelMultiplier: 0.7,
+  infinite: false,
+  gestureOrientation: "vertical",
+  normalizeWheel: false,
+  smoothTouch: false,
+  syncTouchLerp: 1,
+  touchMultiplier: 0.1,
+  autoResize: true,
+});
 
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+function connectToScrollTrigger() {
+  lenis.on("scroll", ScrollTrigger.update);
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+  gsap.ticker.lagSmoothing(0);
+}
+// Uncomment this if using GSAP ScrollTrigger
+connectToScrollTrigger();
+
+window.addEventListener("resize", function () {
+  lenis.resize();
+});
+new WOW().init();
 window.onload = function () {
   innerloader.init();
-  //   setTimeout(function () {
-  //     $(".article-block").delay(2000).scrollClass();
-  //   }, 2000);
+  $(".nav-links a").each(function () {
+    console.log($(".nav-links a").attr("href"), location.pathname);
+    if ($(this).attr("href") == location.pathname) {
+      $(this).addClass("w--current").siblings().removeClass("w--current");
+    }
+  });
 };
 
 gsap.registerPlugin(ScrollTrigger);
